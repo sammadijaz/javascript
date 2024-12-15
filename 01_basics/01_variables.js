2f895b4b1b2b80808f14a3e19d7a1f3e64389ad6
Sammad Ijaz <sammadijaz11223@gmail.com>
2024-12-15 17:16:29 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,31 @@
+// Defining some variables/constants and displaying them
+
+const constantAssign = 123 // This is a constant variable and it cant be changed
+
+let userName = "Sam"
+
+var userAge = "19" //' var ' should not be used, instead ' let ' is supposed to be used
+
+userCity = "Islamabad"
+
+console.log(constantAssign, userName, userAge, userCity);
+
+// Now testing them by changing them now
+
+// constantAssign = 456 //This cant be done as its a constant
+
+userName = "Nina"
+
+userAge = "23"
+
+userCity = "Lahore"
+
+// Using a table to display the output
+console.table([constantAssign, userName, userAge, userCity]) 
+
+/* This program would give an error because we tried to change the constant, so I am going to comment it out and run the program again (Also this is multi-line comment. If I remeber correctly, its the same as 'C' comments) */
+
+/* After commenting out the line trying to change the comment, the program works fine and we can see all other variables are changed to the new values.
+
+We dont use the ' var ' variable because the history of JavaScript not being able to run scope ( ' { } ' these curly brackets are called scopes. Examples:  The ones being used in ' IF ' and ' FOR ' loops etc ) */
+
