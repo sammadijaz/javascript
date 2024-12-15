40ab39436465c93795311e1634c35f0d3837440e
Sammad Ijaz <sammadijaz11223@gmail.com>
2024-12-15 17:29:36 +0000

    container config done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -6,9 +6,11 @@ let userName = "Sam"
 
 var userAge = "19" //' var ' should not be used, instead ' let ' is supposed to be used
 
-userCity = "Islamabad"
+userCity = "Islamabad" // We can define variables like this sometimes but we shouldnt be assigning variables like this
 
-console.log(constantAssign, userName, userAge, userCity);
+let userBalance; // Its upto us whether to put semi-colon or not, it doesnt matter. Also, we can left variables unattended like this and JavaScript would call them as "undefined"
+
+console.log(constantAssign, userName, userAge, userCity, userBalance);
 
 // Now testing them by changing them now
 
@@ -21,7 +23,7 @@ userAge = "23"
 userCity = "Lahore"
 
 // Using a table to display the output
-console.table([constantAssign, userName, userAge, userCity]) 
+console.table([constantAssign, userName, userAge, userCity, userBalance]) 
 
 /* This program would give an error because we tried to change the constant, so I am going to comment it out and run the program again (Also this is multi-line comment. If I remeber correctly, its the same as 'C' comments) */
 
