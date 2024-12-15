943f73a73bf966810b59a8af918eee3bc3abe42d
Sammad Ijaz <sammadijaz11223@gmail.com>
2024-12-15 17:52:06 +0000

    container config done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,12 @@
+"use strict"; // Asking to treat our written code in JS as the newer version of JS
+
+/*  
+
+A while agp, JavaScript was not like the its is as of now. It was updated and classes etc were added into it.
+
+So there was a lot of code written in the older version. The team behind JS did not want it to stop working so they made it workable aswell. This was a little background behind ' "use strict"; '  
+
+*/
+
+
+//  alert(3+3)  // This wont work because we are using nodejs not a browser, it will work in the console of a browser.
